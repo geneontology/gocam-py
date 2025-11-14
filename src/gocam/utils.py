aa658c5156b3e465b0123d23b9021576c5a76a25
Patrick Kalita <pkalita@lbl.gov>
2025-11-13 17:25:44 -0800

    Restore dropped commas

--- src/gocam/utils.py
+++ src/gocam/utils.py
@@ -1,24 +1,24 @@
 # Derived from:
 # https://github.com/geneontology/web-components/blob/5d87e593121eafe6ac4690fa4591f88aa5a03fd8/packages/web-components/src/globals/%40noctua.form/data/taxon-dataset.json
 SPECIES_CODES = [
-    "Atal"
-    "Btau"
-    "Cele"
-    "Cfam"
-    "Ddis"
-    "Dmel"
-    "Drer"
-    "Ggal"
-    "Hsap"
-    "Mmus"
-    "Pseudomonas"
-    "Rnor"
-    "Scer"
-    "Sjap"
-    "Solanaceae"
-    "Spom"
-    "Sscr"
-    "Xenopus"
+    "Atal",
+    "Btau",
+    "Cele",
+    "Cfam",
+    "Ddis",
+    "Dmel",
+    "Drer",
+    "Ggal",
+    "Hsap",
+    "Mmus",
+    "Pseudomonas",
+    "Rnor",
+    "Scer",
+    "Sjap",
+    "Solanaceae",
+    "Spom",
+    "Sscr",
+    "Xenopus",
 ]
 
 
