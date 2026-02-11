721f94abb4c7f55ccbd1f7118f076326858f3798
Patrick Kalita <pkalita@lbl.gov>
2026-02-10 17:06:54 -0800

    Populate flattened contributor and evidence code attributes

--- tests/test_indexing/test_indexer.py
+++ tests/test_indexing/test_indexer.py
@@ -271,3 +271,55 @@ def test_indexer_populates_flattened_provided_by():
         "Test Group 4",
         "Test Group 5",
     }
+
+
+def test_indexer_populates_flattened_contributors():
+    """Test that the indexer populates flattened_contributors correctly."""
+    with open(
+        INPUT_DIR / "test_indexer_populates_flattened_contributors_model.yaml"
+    ) as f:
+        model = Model.model_validate(yaml.safe_load(f))
+
+    indexer = Indexer()
+    indexer.index_model(model)
+
+    assert model.query_index is not None
+    assert model.query_index.flattened_contributors is not None
+    assert len(model.query_index.flattened_contributors) == 8
+    assert {
+        contributor for contributor in model.query_index.flattened_contributors
+    } == {
+        "https://example.org/0000-0000-0000-0001",
+        "https://example.org/0000-0000-0000-0002",
+        "https://example.org/0000-0000-0000-0003",
+        "https://example.org/0000-0000-0000-0004",
+        "https://example.org/0000-0000-0000-0005",
+        "https://example.org/0000-0000-0000-0006",
+        "https://example.org/0000-0000-0000-0007",
+        "https://example.org/0000-0000-0000-0008",
+    }
+
+
+def test_indexer_populates_flattened_evidence_terms():
+    """Test that the indexer populates flattened_evidence_terms correctly."""
+    with open(
+        INPUT_DIR / "test_indexer_populates_flattened_evidence_terms_model.yaml"
+    ) as f:
+        model = Model.model_validate(yaml.safe_load(f))
+
+    indexer = Indexer()
+    indexer.index_model(model)
+
+    assert model.query_index is not None
+    assert model.query_index.flattened_evidence_terms is not None
+    assert len(model.query_index.flattened_evidence_terms) == 2
+    assert {evidence.id for evidence in model.query_index.flattened_evidence_terms} == {
+        "ECO:0000001",
+        "ECO:0000002",
+    }
+    assert {
+        evidence.label for evidence in model.query_index.flattened_evidence_terms
+    } == {
+        "Test Evidence 1",
+        "Test Evidence 2",
+    }
